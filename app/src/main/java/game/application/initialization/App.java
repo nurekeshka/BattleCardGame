90283e8c2f6c8b63e29a87e9bf5b29593dddb0e3
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-03 12:08:37 +0200

    feat: add application entry point

---
+++ app/src/main/java/game/application/initialization/App.java
@@ -0,0 +1,10 @@
+package game.application.initialization;
+
+import game.presentation.MainFrame;
+
+public class App {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.initialize();
+    }
+}
