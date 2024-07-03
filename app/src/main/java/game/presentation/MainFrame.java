ca46f80929abaa2eff56fb0b8d6ba3e78083bfcc
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-03 12:08:27 +0200

    feat: add main frame with temporary content

---
+++ app/src/main/java/game/presentation/MainFrame.java
@@ -0,0 +1,89 @@
+package game.presentation;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
+public class MainFrame extends JFrame {
+    private final Font mainFont = new Font("Segoe print", Font.BOLD, 18);
+
+    JTextField tfFirstName;
+    JTextField tfLastName;
+
+    JLabel lbWelcome;
+
+    public void initialize() {
+        JLabel lbFirstName = new JLabel("First name");
+        lbFirstName.setFont(mainFont);
+
+        tfFirstName = new JTextField();
+        tfFirstName.setFont(mainFont);
+
+        JLabel lbLastName = new JLabel("Last name");
+        lbLastName.setFont(mainFont);
+
+        tfLastName = new JTextField();
+        tfLastName.setFont(mainFont);
+
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(4, 1, 5, 5));
+        formPanel.add(lbFirstName);
+        formPanel.add(tfFirstName);
+        formPanel.add(lbLastName);
+        formPanel.add(tfLastName);
+
+        lbWelcome = new JLabel();
+        lbWelcome.setFont(mainFont);
+
+        JButton btnOK = new JButton("OK");
+        btnOK.setFont(mainFont);
+        btnOK.addActionListener((ActionEvent e) -> {
+            String firstName = tfFirstName.getText();
+            String lastName = tfLastName.getText();
+            lbWelcome.setText("Hello " + firstName + " " + lastName);
+        });
+
+        JButton btnClear = new JButton("Clear");
+        btnClear.setFont(mainFont);
+        btnClear.addActionListener((ActionEvent e) -> {
+            tfFirstName.setText("");
+            tfLastName.setText("");
+            lbWelcome.setText("");
+        });
+
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout(1, 2, 5, 5));
+        buttonsPanel.add(btnOK);
+        buttonsPanel.add(btnClear);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBackground(new Color(128, 128, 255));
+        mainPanel.add(formPanel, BorderLayout.NORTH);
+        mainPanel.add(lbWelcome, BorderLayout.CENTER);
+        mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
+
+        add(mainPanel);
+
+        setTitle("Welcome");
+        setSize(500, 600);
+        setMinimumSize(new Dimension(300, 400));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.initialize();
+    }
+}
