257faaca04040c62e382c84ae68c6911ec995fe6
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-04 15:43:13 +0200

    feat: add main menu panel

---
+++ app/src/main/java/game/presentation/components/panels/MenuPanel.java
@@ -0,0 +1,10 @@
+package game.presentation.components.panels;
+
+import javax.swing.JPanel;
+
+public class MenuPanel extends JPanel {
+    public MenuPanel() {
+        super();
+        // Configure the menu panel here
+    }
+}
