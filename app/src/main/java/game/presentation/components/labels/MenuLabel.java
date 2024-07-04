258316e25f67d06da3b2e25572bfd4332931a79f
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-04 15:42:59 +0200

    feat: add simple menu label

---
+++ app/src/main/java/game/presentation/components/labels/MenuLabel.java
@@ -0,0 +1,10 @@
+package game.presentation.components.labels;
+
+import javax.swing.JLabel;
+
+public class MenuLabel extends JLabel {
+    public MenuLabel() {
+        super();
+        // Configure menu label here
+    }
+}
