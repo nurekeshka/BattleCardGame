796fa97d4f1291918736b6a74adb220a64972a15
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-04 14:34:23 +0200

    feat: add game logic interface and implementation sketch

---
+++ app/src/main/java/game/infrastructure/logic/GameLogic.java
@@ -0,0 +1,8 @@
+package game.infrastructure.logic;
+
+public interface GameLogic {
+    // This method should initialize the game process
+    public void start();
+
+    public void next();
+}
---
+++ app/src/main/java/game/infrastructure/logic/impl/GameLogicImpl.java
@@ -0,0 +1,17 @@
+package game.infrastructure.logic.impl;
+
+import game.infrastructure.logic.GameLogic;
+
+public class GameLogicImpl implements GameLogic {
+
+    @Override
+    public void start() {
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void next() {
+        throw new UnsupportedOperationException("Unimplemented method 'next'");
+    }
+
+}
