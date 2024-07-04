a4eeace49912a065118e3b3bc70f19b13eb934e2
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-04 15:42:53 +0200

    feat: add game button sketches

---
+++ app/src/main/java/game/presentation/components/buttons/GameNextButton.java
@@ -0,0 +1,10 @@
+package game.presentation.components.buttons;
+
+import javax.swing.JButton;
+
+public class GameNextButton extends JButton {
+    public GameNextButton() {
+        super();
+        // Configure game button presentation and logic here
+    }
+}
---
+++ app/src/main/java/game/presentation/components/buttons/MenuButton.java
@@ -0,0 +1,10 @@
+package game.presentation.components.buttons;
+
+import javax.swing.JButton;
+
+public class MenuButton extends JButton {
+    public MenuButton() {
+        super();
+        // Configure the button presentation and logic here
+    }
+}
