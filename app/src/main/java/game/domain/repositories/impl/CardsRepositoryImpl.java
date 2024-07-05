7ad62509be29cd8731fe48b6f9beb56086dde4e9
Amirali-83 <amirali.sotoudeh@yahoo.com>
2024-07-05 16:21:04 +0200

    feat: add

--- app/src/main/java/game/domain/repositories/impl/CardsRepositoryImpl.java
+++ app/src/main/java/game/domain/repositories/impl/CardsRepositoryImpl.java
@@ -6,18 +6,26 @@ import game.domain.models.Card;
 import game.domain.models.Deck;
 import game.domain.repositories.CardsRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardsRepositoryImpl implements CardsRepository {
 
     @Override
     public Card getCard(CardSuit suit, CardValue value) {
-        throw new UnsupportedOperationException("Unimplemented method 'getCard'");
+        return new Card(suit, value);
     }
 
     @Override
     public Deck getFullDeck() {
+        List<Card> cards = new ArrayList<>();
 
-        System.out.print("hello");
-        return null;
-    }
+        for (CardSuit suit : CardSuit.values()) {
+            for (CardValue value : CardValue.values()) {
+                cards.add(new Card(suit, value));
+            }
+        }
 
+        return new Deck(cards);
+    } 
 }
