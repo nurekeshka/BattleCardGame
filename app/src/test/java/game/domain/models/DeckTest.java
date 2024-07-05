7f0af23188aaea85e29cbb568074531f937a7eac
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-05 19:16:35 +0200

    fix: tests according to the new implementation

--- app/src/test/java/game/domain/models/DeckTest.java
+++ app/src/test/java/game/domain/models/DeckTest.java
@@ -3,32 +3,47 @@ package game.domain.models;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import game.application.controls.Guice;
+import game.application.controls.Injector;
+import game.application.initialization.BasicModule;
 import game.domain.enums.CardSuit;
 import game.domain.enums.CardValue;
+import game.domain.repositories.CardsRepository;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DeckTest {
+    private Injector injector;
+    private CardsRepository cardsRepository;
+
     Card cardMock = new Card(CardSuit.CLUBS, CardValue.ACE);
     Card[] cardsArrayMock = new Card[] { cardMock };
 
+    @BeforeAll
+    public void init() {
+        this.injector = Guice.createInjector(new BasicModule());
+        this.cardsRepository = this.injector.getInstance(CardsRepository.class);
+    }
+
     @Test
     void initializationTest() {
-        Deck deck = new Deck(this.cardsArrayMock);
-        assertEquals(1, deck.getCards().length);
+        Deck deck = new Deck(new Card[] {});
+        assertEquals(0, deck.getCards().length);
     }
 
     @Test
     void addingCardOnTopTest() {
-        Deck deck = new Deck(this.cardsArrayMock);
-        deck.addCardOnTop(cardMock);
-        assertEquals(2, deck.getCards().length);
+        Deck deck = this.cardsRepository.getFullDeck();
+        assertEquals(52, deck.getCards().length);
     }
 
     @Test
     void takingCardFromBottomTest() {
         Deck deck = new Deck(this.cardsArrayMock);
-        Card card = deck.takeCardFromBottom();
+        Card card = deck.takeCardsFromBottom();
         assertNotNull(card);
         assertEquals(0, deck.getCards().length);
     }
