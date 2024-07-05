f8ec9c023f2c471339d6261f0f216a3e911405f5
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-05 21:27:34 +0200

    fix: remove unnecessary import

--- app/src/main/java/game/presentation/frames/GameFrame.java
+++ app/src/main/java/game/presentation/frames/GameFrame.java
@@ -9,7 +9,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.FlowLayout;
 import java.awt.Image;
 
 import javax.swing.JLabel;
@@ -51,7 +50,7 @@ public class GameFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		
+
 		JButton btnNextTurn = new JButton("Next Turn");
 		btnNextTurn.setBounds(325, 325, 150, 50);
 		btnNextTurn.addActionListener(new ActionListener() {
@@ -59,76 +58,78 @@ public class GameFrame extends JFrame {
 			}
 		});
 		contentPane.setLayout(null);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 786, 25);
 		contentPane.add(menuBar);
-		
+
 		JMenu mnAbout = new JMenu("About");
 		menuBar.add(mnAbout);
-		
+
 		JMenuItem mntmMenuItem1 = new JMenuItem("Code: Name(s)");
 		mnAbout.add(mntmMenuItem1);
-		
+
 		JMenuItem mntmMenuItem2 = new JMenuItem("UI: Name(s)");
 		mnAbout.add(mntmMenuItem2);
-		
+
 		JMenuItem mntmMenuItem3 = new JMenuItem("Images: Name(s)");
 		mnAbout.add(mntmMenuItem3);
 		contentPane.add(btnNextTurn);
-		
-		//IMPORTING IMAGES HERE
-		ImageIcon imgJ = new ImageIcon("PLACEHOLDER_CARD_IMAGE.png"); //Full Size - Deck
-		Image imgTemp = imgJ.getImage().getScaledInstance(imgJ.getIconWidth()/2, imgJ.getIconHeight()/2, java.awt.Image.SCALE_SMOOTH);
-		ImageIcon imgJS = new ImageIcon(imgTemp); //Half Size - War
-		
+
+		// IMPORTING IMAGES HERE
+		ImageIcon imgJ = new ImageIcon("PLACEHOLDER_CARD_IMAGE.png"); // Full Size - Deck
+		Image imgTemp = imgJ.getImage().getScaledInstance(imgJ.getIconWidth() / 2, imgJ.getIconHeight() / 2,
+				java.awt.Image.SCALE_SMOOTH);
+		ImageIcon imgJS = new ImageIcon(imgTemp); // Half Size - War
+
 		JLabel lblDeck_P1 = new JLabel("");
 		lblDeck_P1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblDeck_P1.setBounds(150, 80, 150, 200);
 		lblDeck_P1.setIcon(imgJ);
 		contentPane.add(lblDeck_P1);
-		
+
 		JLabel lblDeck_P2 = new JLabel("");
 		lblDeck_P2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblDeck_P2.setBounds(500, 80, 150, 200);
 		lblDeck_P2.setIcon(imgJ);
 		contentPane.add(lblDeck_P2);
-		
+
 		JLabel lblTotal_P1 = new JLabel("Total Cards Count");
 		lblTotal_P1.setFont(new Font("Tahoma", Font.PLAIN, 8));
 		lblTotal_P1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTotal_P1.setBounds(20, 180, 100, 20);
 		contentPane.add(lblTotal_P1);
-		
+
 		JLabel lblTotal_P2 = new JLabel("Total Cards Count");
 		lblTotal_P2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTotal_P2.setFont(new Font("Tahoma", Font.PLAIN, 8));
 		lblTotal_P2.setBounds(680, 180, 100, 20);
 		contentPane.add(lblTotal_P2);
-		
+
 		JLabel lblTotalN_P1 = new JLabel("N");
 		lblTotalN_P1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTotalN_P1.setBounds(20, 200, 100, 20);
 		contentPane.add(lblTotalN_P1);
-		
+
 		JLabel lblTotalN_P2 = new JLabel("N");
 		lblTotalN_P2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTotalN_P2.setBounds(680, 200, 100, 20);
 		contentPane.add(lblTotalN_P2);
-		
+
 		JLabel lblWar_P1 = new JLabel("");
 		lblWar_P1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblWar_P1.setBounds(310, 130, 75, 100);
 		lblWar_P1.setIcon(imgJS);
 		contentPane.add(lblWar_P1);
-		
+
 		JLabel lblWar_P2 = new JLabel("");
 		lblWar_P2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblWar_P2.setBounds(415, 130, 75, 100);
 		lblWar_P2.setIcon(imgJS);
 		contentPane.add(lblWar_P2);
-		
-		JLabel lblLogs = new JLabel("Logs: This shows the most recent action taken for better clarity and debug purposes");
+
+		JLabel lblLogs = new JLabel(
+				"Logs: This shows the most recent action taken for better clarity and debug purposes");
 		lblLogs.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblLogs.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		lblLogs.setBounds(475, 35, 300, 20);
