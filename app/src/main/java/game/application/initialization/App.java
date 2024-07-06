0c567a1ca9b9b607a90def893d7536d186fe4741
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-06 14:30:16 +0200

    fix: application entry point

--- app/src/main/java/game/application/initialization/App.java
+++ app/src/main/java/game/application/initialization/App.java
@@ -1,9 +1,13 @@
 package game.application.initialization;
 
+import game.application.controls.Guice;
+import game.application.controls.Injector;
 import game.presentation.frames.GameFrame;
 
 public class App {
     public static void main(String[] args) {
-        GameFrame.main(args);
+        Injector injector = Guice.createInjector(new BasicModule());
+        GameFrame frame = injector.getInstance(GameFrame.class);
+        frame.setVisible(true);
     }
 }
