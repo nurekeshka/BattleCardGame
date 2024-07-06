5daae40d76fcf8befb8fa27028e4bd3fcf5d98c5
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-06 14:21:02 +0200

    feat: add getImagePath method to cards repository implementation

--- app/src/main/java/game/domain/repositories/impl/CardsRepositoryImpl.java
+++ app/src/main/java/game/domain/repositories/impl/CardsRepositoryImpl.java
@@ -8,15 +8,10 @@ import game.domain.repositories.CardsRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.awt.Image;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CardsRepositoryImpl implements CardsRepository {
-
-    private static final String IMAGE_PATH_TEMPLATE = "path/to/images/%s_%s.png"; //AmirAli's part added to game-frame branch
-
     @Override
     public Card getCard(CardSuit suit, CardValue value) {
         return new Card(suit, value);
@@ -35,25 +30,9 @@ public class CardsRepositoryImpl implements CardsRepository {
         return new Deck(cards.toArray(new Card[0]));
     }
 
-    private String getResourcePath(CardSuit suit, CardValue value) {
-        return String.format("/images/%s_%s.png", suit.toString().toLowerCase(), value.toString().toLowerCase());
+    @Override
+    public Path getImagePath(Card card) {
+        return Paths.get("src", "main", "resources", card.getSuit().toString().toLowerCase(),
+                String.format("%d.png", card.getValue().getValue()));
     }
-
-    private Image loadImage(String imagePath) {
-        try (InputStream is = getClass().getResourceAsStream(imagePath)) {
-            if (is != null) {
-                return ImageIO.read(is);
-            } else {
-                System.err.println("Image not found for path: " + imagePath);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private String getImagePath(Card card) { //AmirAli's part added to game-frame branch
-        return String.format(IMAGE_PATH_TEMPLATE, card.getSuit().toString().toLowerCase(), card.getValue().toString().toLowerCase());
-    }
-
 }
