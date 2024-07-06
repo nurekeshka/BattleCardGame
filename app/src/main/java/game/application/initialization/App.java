1e54a32dd1f54bc947637556de20bf7106e0124e
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-06 14:46:35 +0200

    feat: add something

--- app/src/main/java/game/application/initialization/App.java
+++ app/src/main/java/game/application/initialization/App.java
@@ -8,6 +8,6 @@ public class App {
     public static void main(String[] args) {
         Injector injector = Guice.createInjector(new BasicModule());
         GameFrame frame = injector.getInstance(GameFrame.class);
-        frame.setVisible(true);
+        frame.init();
     }
 }
