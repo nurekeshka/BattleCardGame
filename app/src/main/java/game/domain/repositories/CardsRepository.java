cace3c55d1141e1ea8ee9f871737fb06261d1df7
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-06 14:20:35 +0200

    feat: add getImagePath method to cards repository interface

--- app/src/main/java/game/domain/repositories/CardsRepository.java
+++ app/src/main/java/game/domain/repositories/CardsRepository.java
@@ -5,8 +5,12 @@ import game.domain.enums.CardValue;
 import game.domain.models.Card;
 import game.domain.models.Deck;
 
+import java.nio.file.Path;
+
 public interface CardsRepository {
     public Card getCard(CardSuit suit, CardValue value);
 
     public Deck getFullDeck();
+
+    public Path getImagePath(Card card);
 }
