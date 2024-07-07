c6c965a558c5153db5ea0998f67fbf540b35ee5d
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-07 16:30:33 +0200

    fix: remove game saver interface methods

--- app/src/main/java/game/infrastructure/progress/GameSaver.java
+++ app/src/main/java/game/infrastructure/progress/GameSaver.java
@@ -1,12 +1,5 @@
 package game.infrastructure.progress;
 
-import game.domain.models.Deck;
-
-import java.io.IOException;
-
 public interface GameSaver {
 
-    public void saveGame(Deck[] gameDecks) throws IOException;
-    public Deck[] loadGame(String filename) throws IOException, ClassNotFoundException;
-    public String dateAndTimeAsString();
 }
