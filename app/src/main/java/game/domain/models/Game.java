271aa57779fa0b534dd9430e08c587b85b9e2d3c
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-07 14:17:55 +0200

    feat: add get player one, two deck

--- app/src/main/java/game/domain/models/Game.java
+++ app/src/main/java/game/domain/models/Game.java
@@ -3,15 +3,31 @@ package game.domain.models;
 import game.domain.enums.GameState;
 
 public class Game {
-    public final Player[] players;
-    public GameState state;
+    private final Player[] players;
+    private GameState state;
 
     public Game(Player[] players) {
         this.players = players;
         this.state = GameState.RUNNING;
     }
 
-    public void updateGameState() {
-        // This class should check if any of the players update game state
+    public void updateGameState(GameState state) {
+        this.state = state;
+    }
+
+    public Deck getPlayerOneDeck() {
+        return this.players[0].getDeck();
+    }
+
+    public Deck getPlayerTwoDeck() {
+        return this.players[1].getDeck();
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public GameState getState() {
+        return state;
     }
 }
