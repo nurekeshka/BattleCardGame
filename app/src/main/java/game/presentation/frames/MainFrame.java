a15059f08e3a62ed2686a826a1569db7d4d112a4
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-07 16:40:50 +0200

    feat: add switch to main frame window

--- app/src/main/java/game/presentation/frames/MainFrame.java
+++ app/src/main/java/game/presentation/frames/MainFrame.java
@@ -5,9 +5,25 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
+import game.domain.enums.GameWindows;
+import game.presentation.components.panels.GamePanel;
+import game.presentation.components.panels.MenuPanel;
+
 public class MainFrame extends JFrame {
     public void init() {
         this.setWindowConfiguration();
+    }
+
+    public void switchToWindow(GameWindows window) {
+        this.getContentPane().removeAll();
+
+        if (window == GameWindows.MENU) {
+            this.getContentPane().add(new MenuPanel());
+        } else if (window == GameWindows.GAME) {
+            this.getContentPane().add(new GamePanel());
+        } else if (window == GameWindows.FINAL) {
+            this.getContentPane().add(new FinalFrame());
+        }
     }
 
     public void setWindowConfiguration() {
