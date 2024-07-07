d8fb531da8ba41f26fc87a40edcb37ecfc2dd0f6
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-07 18:21:57 +0200

    fix: menu panel

--- app/src/main/java/game/presentation/components/panels/MenuPanel.java
+++ app/src/main/java/game/presentation/components/panels/MenuPanel.java
@@ -12,20 +12,17 @@ public class MenuPanel extends JPanel {
     String[] buttons = new String[] { "Play", "Exit" };
 
     public MenuPanel init(MainFrame main) {
-        JPanel menuPanel = new JPanel();
+        setLayout(new GridLayout(this.buttons.length, 1, 10, 10));
 
         JButton playButton = new JButton(buttons[0]);
         JButton exitButton = new JButton(buttons[1]);
 
-        menuPanel.setLayout(new GridLayout(this.buttons.length, 1, 10, 10));
-
-        menuPanel.add(playButton);
-        menuPanel.add(exitButton);
-
         playButton.addActionListener((ActionEvent e) -> main.switchToPanel(GamePanels.GAME));
         exitButton.addActionListener((ActionEvent e) -> System.exit(0));
 
-        this.add(menuPanel);
+        this.add(playButton);
+        this.add(exitButton);
+
         return this;
     }
 }
