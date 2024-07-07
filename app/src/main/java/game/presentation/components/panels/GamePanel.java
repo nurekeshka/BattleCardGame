761328a40c7414d158aeb92d10132e8034d8c291
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-07 22:00:22 +0200

    fix: game panel

--- app/src/main/java/game/presentation/components/panels/GamePanel.java
+++ app/src/main/java/game/presentation/components/panels/GamePanel.java
@@ -2,29 +2,27 @@ package game.presentation.components.panels;
 
 import javax.swing.JPanel;
 
-import game.domain.enums.CardSuit;
-import game.domain.enums.CardRank;
-import game.domain.models.Card;
-import game.domain.models.Game;
-import game.domain.repositories.CardsRepository;
-import game.infrastructure.logic.GameLogic;
 import game.infrastructure.progress.GameProgress;
+import game.infrastructure.logic.GameLogic;
+import game.domain.repositories.CardsRepository;
+import game.domain.models.Card;
 
 import javax.swing.border.EmptyBorder;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+
 import java.awt.event.ActionEvent;
-import java.awt.Font;
 import java.awt.GridLayout;
-import java.nio.file.Path;
 import java.awt.Image;
+import java.awt.Font;
 
-import javax.swing.JLabel;
 import javax.swing.SwingConstants;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JMenuBar;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
 
+import game.presentation.components.images.CardImage;
 import game.presentation.components.labels.PlayerDeckLabel;
 import game.presentation.components.labels.TotalCardsLabel;
 import game.presentation.components.labels.TotalCardsViewLabel;
@@ -32,7 +30,6 @@ import game.presentation.frames.MainFrame;
 
 public class GamePanel extends JPanel {
     private static final Font mainFont = new Font("Tahoma", Font.PLAIN, 8);
-    private static final Font loggerFont = new Font("Tahoma", Font.PLAIN, 7);
     private static final long serialVersionUID = 1L;
 
     private final transient GameLogic gameLogic;
@@ -82,34 +79,47 @@ public class GamePanel extends JPanel {
 
     public void updateFromGameState() {
         this.clearFrame();
+        this.setNextTurnButton();
+        this.setLayout(null);
+        this.setTotalCardsLabels();
+        this.setWarBattleLabel();
+    }
 
-        Game gameObject = this.gameLogic.getGameObject();
-
-        JButton btnNextTurn = new JButton("Next Turn");
-        btnNextTurn.setBounds(325, 325, 150, 50);
-        btnNextTurn.addActionListener((ActionEvent e) -> gameLogic.next());
-        setLayout(null);
-        add(btnNextTurn);
-
-        Card card = new Card(CardSuit.HEARTS, CardRank.ACE);
-        Path path = cardsRepository.getImagePath(card);
-
-        ImageIcon img1 = new ImageIcon(path.toString()); // Full Size - Deck
-        Image imgTemp;
-        imgTemp = img1.getImage().getScaledInstance(150, 200, Image.SCALE_SMOOTH);
-        img1 = new ImageIcon(imgTemp);
-        imgTemp = img1.getImage().getScaledInstance(img1.getIconWidth() / 2, img1.getIconHeight() / 2,
-                Image.SCALE_SMOOTH);
-        ImageIcon img1Small = new ImageIcon(imgTemp); // Half Size - War
-
-        JLabel playerOneDeckLabel = new PlayerDeckLabel(img1);
+    public void setPlayerDeckLabels() {
+        JLabel playerOneDeckLabel = new PlayerDeckLabel(
+                this.getCardImage(this.gameLogic.getBattlingCardLeft(), 150, 200));
         playerOneDeckLabel.setBounds(150, 80, 150, 200);
         add(playerOneDeckLabel);
 
-        JLabel playerTwoDeckLabel = new PlayerDeckLabel(img1);
+        JLabel playerTwoDeckLabel = new PlayerDeckLabel(
+                this.getCardImage(this.gameLogic.getBattlingCardRight(), 150, 200));
         playerTwoDeckLabel.setBounds(500, 80, 150, 200);
-        add(playerTwoDeckLabel);
 
+        add(playerTwoDeckLabel);
+    }
+
+    public void setNextTurnButton() {
+        JButton nextTurnButton = new JButton("Next Turn");
+        nextTurnButton.setBounds(325, 325, 150, 50);
+        nextTurnButton.addActionListener((ActionEvent e) -> {
+            gameLogic.next();
+            this.updateFromGameState();
+        });
+
+        add(nextTurnButton);
+    }
+
+    public void setWarBattleLabel() {
+        JLabel warLabel = new JLabel();
+        ImageIcon cover = this.getCardImage(new Card(null, null), 150, 200);
+
+        warLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        warLabel.setBounds(362, 130, 75, 100);
+        warLabel.setIcon(cover);
+        add(warLabel);
+    }
+
+    public void setTotalCardsLabels() {
         JLabel playerOneTotalLabel = new TotalCardsLabel(mainFont);
         playerOneTotalLabel.setBounds(20, 180, 100, 20);
         add(playerOneTotalLabel);
@@ -125,19 +135,6 @@ public class GamePanel extends JPanel {
         JLabel playerTwoCardsLabel = new TotalCardsViewLabel(this.gameLogic.getPlayerTwoDeckCardsCount());
         playerTwoCardsLabel.setBounds(680, 200, 100, 20);
         add(playerTwoCardsLabel);
-
-        JLabel warLabel = new JLabel("");
-        warLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        warLabel.setBounds(362, 130, 75, 100);
-        warLabel.setIcon(img1Small);
-        add(warLabel);
-
-        JLabel lblLogs = new JLabel(
-                "Logs: This shows the most recent action taken for better clarity and debug purposes");
-        lblLogs.setHorizontalAlignment(SwingConstants.RIGHT);
-        lblLogs.setFont(loggerFont);
-        lblLogs.setBounds(475, 35, 300, 20);
-        add(lblLogs);
     }
 
     public void setMenuConfiguration() {
@@ -155,8 +152,13 @@ public class GamePanel extends JPanel {
         this.add(menu);
     }
 
-    public ImageIcon getImage(Card card) {
-        return new ImageIcon(this.cardsRepository.getImagePath(card).toString());
+    public ImageIcon getCardImage(Card card) {
+        return new CardImage(this.cardsRepository.getImagePath(card));
+    }
+
+    public ImageIcon getCardImage(Card card, int width, int height) {
+        CardImage image = new CardImage(this.cardsRepository.getImagePath(card));
+        return new ImageIcon(image.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
     }
 
     public void clearFrame() {
