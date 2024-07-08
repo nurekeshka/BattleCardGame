a61652e9b0a65836007a498df0edceecba89c164
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-08 09:27:35 +0200

    feat: add change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("org.beryx.jlink") version "3.0.1"
 }
 
 repositories {
